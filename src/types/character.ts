58a324c25454136ca60d12534ef14e12882db55e
yuuki-cfeebm <kauayuuki10@gmail.com>
2026-02-09 00:58:50 -0300

    refactor: altera tipagem de personagens

--- src/types/character.ts
+++ src/types/character.ts
@@ -1,25 +1,39 @@
 export type Character = {
-  id: string,
-  name: string,
+  id: string
+  name: string
+  powerstats: {
+    intelligence: string
+    strength: string
+    speed: string
+    durability: string
+    power: string
+    combat: string
+  }
+  biography: {
+    'full-name': string
+    'place-of-birth': string
+    'first-appearance': string
+    publisher: string
+    alignment: string
+  }
+  appearance: {
+    gender: string
+    race: string
+    height: string[]
+    weight: string[]
+    eyeColor: string
+    hairColor: string
+  }
+  work: {
+    occupation: string
+    base: string
+  }
+  connections: {
+    'group-affiliation': string
+    relatives: string
+  }
   image: {
     url: string
-  },
-  biography: {
-    fullName: string,
   }
-  powerstats: {
-    intelligence: string,
-    strength: string,
-    speed: string,
-    durability: string,
-    power: string,
-    combat: string
-  },
-  appearance: {
-    gender: string,
-    race: string | null,
-    height: string[],
-    weight: string[]
-  },
 }
 						
